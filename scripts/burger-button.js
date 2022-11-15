4199a0d27b7e49a6b1f5adc811a671fa48feaaef
melnikovmaxim <melnikovmaxim.nhk@gmail.com>
2022-11-15 20:17:04 +1000

    доделал burger menu + button

--- scripts/burger-button.js
+++ scripts/burger-button.js
@@ -1,5 +1,7 @@
 const burgerButton = document.getElementsByClassName('content-header__burger-button')[0];
 const modal = document.getElementsByClassName('modal')[0];
+const nav = document.getElementsByClassName('content-header__nav')[0];
+const burgerButtonInner = document.getElementsByClassName('burger-button__inner')[0];
 
 let isToggled = false;
 
@@ -8,8 +10,12 @@ const handleClick = () => {
 
     if (isToggled) {
         modal.classList.add('modal--visible');
+        nav.classList.add('content-header__nav--visible');
+        burgerButtonInner.classList.add('burger-button__inner--toggled');
     } else {
-        modal.classList.remove('modal--visible')
+        modal.classList.remove('modal--visible');
+        nav.classList.remove('content-header__nav--visible');
+        burgerButtonInner.classList.remove('burger-button__inner--toggled');
     }
 }
 
