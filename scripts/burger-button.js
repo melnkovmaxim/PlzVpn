5d2ee0b3ad316cafa7e4590a9a78e923c8ca8a32
melnikovmaxim <melnikovmaxim.nhk@gmail.com>
2022-11-16 21:10:59 +1000

    pixel-perfect desktop

--- scripts/burger-button.js
+++ scripts/burger-button.js
@@ -1,6 +1,6 @@
-const burgerButton = document.getElementsByClassName('content-header__burger-button')[0];
+const burgerButton = document.getElementsByClassName('header__burger-button')[0];
 const modal = document.getElementsByClassName('modal')[0];
-const nav = document.getElementsByClassName('content-header__nav')[0];
+const nav = document.getElementsByClassName('header__nav')[0];
 const burgerButtonInner = document.getElementsByClassName('burger-button__inner')[0];
 
 let isToggled = false;
@@ -9,12 +9,14 @@ const handleClick = () => {
     isToggled = !isToggled;
 
     if (isToggled) {
+        document.body.style.overflow = 'hidden';
         modal.classList.add('modal--visible');
-        nav.classList.add('content-header__nav--visible');
+        nav.classList.add('header__nav--visible');
         burgerButtonInner.classList.add('burger-button__inner--toggled');
     } else {
+        document.body.style.overflow = 'show';
         modal.classList.remove('modal--visible');
-        nav.classList.remove('content-header__nav--visible');
+        nav.classList.remove('header__nav--visible');
         burgerButtonInner.classList.remove('burger-button__inner--toggled');
     }
 }
